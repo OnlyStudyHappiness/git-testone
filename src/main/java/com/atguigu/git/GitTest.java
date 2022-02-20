101e1f4b9a8fd9257db985327cb4a77257a657db
OnlyStudyHappiness <100074432+OnlyStudyHappiness@users.noreply.github.com>
2022-02-20 20:42:00 +0800

    Update GitTest.java
    twice

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello,git4!");
         System.out.println("Push!!!");
         System.out.println("Pull!!!");
+        System.ou.println("hahaha success");
     }
 }
